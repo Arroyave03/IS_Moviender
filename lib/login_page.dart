db163c1bfafe0bb04e7ad3e6fc5dc4fceca20618
Arroyave03 <arroyave0322@hotmail.com>
2023-10-03 21:14:35 -0500

    feat: login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,21 +14,42 @@ class LoginPage extends StatelessWidget {
             'MOVIENDER',
             textAlign: TextAlign.center,
             style: TextStyle(
+              fontFamily: 'openSans',
               fontSize: 35,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          const Spacer(flex: 2),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Ingresa tu ID',
+          const Spacer(flex: 3),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'ID',
+                contentPadding: EdgeInsets.fromLTRB(10, 10, 0, 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+              ),
             ),
           ),
           const SizedBox(height: 30),
           ElevatedButton(
             onPressed: () {},
-            child: Text("Login"),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.pink[500],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            child: const Text("Login"),
           ),
-          const Spacer(flex: 3),
+          const Spacer(flex: 2),
         ],
       ),
     );
